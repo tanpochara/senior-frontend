8e38fdbf6c35c92c39845e7411306c0fbe5c19ca
Pochara Youcharoen <pochara.youcharoen@agoda.com>
2023-10-10 01:31:18 +0700

    Feat: toogle theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
 'use client'
-import './global.css'
-import { useEffect } from 'react'
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { ThemeProvider } from "next-themes";
+import "./global.css"
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  useEffect(() => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  },[])
-
   return (
     <html lang="en">
-      <body className='bg-white'>{children}</body>
+      <body
+        className={`${inter.className} bg-slate-50 dark:bg-[#0d1117] ${1}`}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeSwitcher />
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,12 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
+"use client";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className='text-primary dark:text-white'>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <>
+      hello world
+    </>
   );
 }
