f7071966e1552d403fad590cf1feb2e9216afcb8
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-26 10:00:11 +0700

    feat: language change

--- src/components/Navbar/LanguageDropdown.tsx
+++ src/components/Navbar/LanguageDropdown.tsx
@@ -4,38 +4,45 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useState } from "react";
 import { Typography } from "../ui/typography";
 import { FaAngleDown } from "react-icons/fa6";
 import Image from "next/image";
+import { usePathname, useRouter } from "@/navigation";
 
 const icons: Record<string, string> = {
-    "en": '/en.svg',
-    "th": '/th.svg'
-}
+  en: "/en.svg",
+  th: "/th.svg",
+};
 
 export const LanguageDropdown: React.FC = () => {
-  const [position, setPosition] = useState("en");
+  const [language, setLanguage] = useState("en");
+  const currentPath = usePathname();
+  const router = useRouter();
+
+  const handleLanguageChange = (value: string) => {
+    setLanguage(value);
+    router.replace(currentPath, {locale: value})
+  }
+    
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="flex flex-row items-center gap-2">
-            <Image src={icons[position]} alt="logo" width={25} height={20} />
+          <Image src={icons[language]} alt="logo" width={25} height={20} />
           <Typography variant="body1" className="font-bold">
-            {position}
+            {language}
           </Typography>
           <FaAngleDown />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 ">
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+        <DropdownMenuRadioGroup value={language} onValueChange={handleLanguageChange}>
           <DropdownMenuRadioItem className="bg-white" value="en">
             english
           </DropdownMenuRadioItem>
