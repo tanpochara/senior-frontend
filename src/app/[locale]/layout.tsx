0824cc02988477c9d7b06b985b35def62b2f7180
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-26 09:59:39 +0700

    feat: integrate i18n

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 import { NavBar } from "@/components/Navbar/NavBar";
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,15 @@
-"use client";
 import { logout } from "@/action/logout";
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/typography";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import {useTranslations} from 'next-intl';
 
 export default function Home() {
-  const { data } = useSession();
+  const t = useTranslations('Index')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Typography variant="h1">Welcome to Next.js</Typography>
+      <Typography variant="h1">{t('title')}</Typography>
     </main>
   );
 }
